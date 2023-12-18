1f9d2a43d4c037bc97e0b34821f72107d728b7f7
Elif Çiftçioğlu <118854294+ElifLu@users.noreply.github.com>
2023-12-18 13:34:33 +0300

    fix

--- api/models/venue.js
+++ api/models/venue.js
@@ -6,7 +6,6 @@ const hour = new mongoose.Schema({
     close: String,
     isClosed: { type: Boolean, required: false }
 });
-
 const comment = new mongoose.Schema({
     author: { type: String, required: true },
     rating: { type: Number, min: 0, max: 5, required: true },
